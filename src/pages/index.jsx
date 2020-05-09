704f307d934ee8616445f6461f2915ae22fb8439
SGarcia710 <sgarcia710@outlook.com>
2020-05-09 01:25:58 -0500

    Add Container components

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Container } from "../components"
+
+const IndexPage = () => {
+  return (
+    <Container>
+      <h1>Hello world</h1>
+    </Container>
+  )
+}
+
+export default IndexPage
